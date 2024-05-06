2062f854429f5f7799312f38e03f74c853e7e180
lynjun <lynjun97@naver.com>
2024-05-06 20:53:30 +0900

    Feat: SaveMemberRequest 추가

--- domain/src/main/java/way/application/domain/member/Member.java
+++ domain/src/main/java/way/application/domain/member/Member.java
@@ -1,7 +1,6 @@
 package way.application.domain.member;
 
 import jakarta.validation.constraints.NotBlank;
-import org.jetbrains.annotations.NotNull;
 
 public class Member {
     public record Request(
@@ -10,6 +9,23 @@ public class Member {
 
             @NotBlank(message = "값을 입력해주세요")
             String password
+    ) {
+
+    }
+
+    public record SaveMemberRequest(
+            @NotBlank(message = "userName을 입력해주세요")
+            String userName,
+
+            @NotBlank(message = "userId를 입력해주세요")
+            String userId,
+
+            @NotBlank(message = "password를 입력해주세요")
+            String password,
+
+            @NotBlank(message = "email를 입력해주세요")
+            String email
+
     ) {
 
     }
