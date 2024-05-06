241b8723ca3582a2c1bb122f682d0e76ae2dfdd8
lynjun <lynjun97@naver.com>
2024-05-06 20:54:41 +0900

    Feat: save 추가

--- data/src/main/java/way/application/data/member/MemberRepositoryImpl.java
+++ data/src/main/java/way/application/data/member/MemberRepositoryImpl.java
@@ -1,9 +1,29 @@
 package way.application.data.member;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import way.application.domain.member.Member;
 import way.application.domain.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepository {
 
+    private final MemberJpaRepository memberJpaRepository;
+    private final BCryptPasswordEncoder encoder;
+    @Override
+    public void save(Member.SaveMemberRequest request) {
+
+        MemberEntity member = MemberEntity.builder()
+                .userName(request.userName())
+                .userId(request.userId())
+                .password(encoder.encode(request.password()))
+                .email(request.email())
+                .profileImage(null)
+                .build();
+
+        memberJpaRepository.save(member);
+
+    }
 }
