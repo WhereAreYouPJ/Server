0af6798f0f6fea0fc70823c5fee7e4d832c89cfb
euics <pjh575157@gmail.com>
2024-05-09 00:47:09 +0900

    docs: Add Swagger javadoc

--- application/src/main/java/way/application/controller/HealthCheckController.java
+++ application/src/main/java/way/application/controller/HealthCheckController.java
@@ -1,11 +1,13 @@
 package way.application.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import way.application.core.base.BaseResponse;
 
 @RestController
+@Tag(name = "Server Health Check", description = "서버 환경 확인을 위한 URL: https://wlrmadjel.com/actuator/health 반환값 200이면 Server OK")
 public class HealthCheckController {
     @GetMapping("/actuator/health")
     public ResponseEntity<BaseResponse> healthCheck() {
