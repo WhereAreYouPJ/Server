374a20af3a9338a6e3e20805e699289ad9db5d32
euics <pjh575157@gmail.com>
2024-05-09 21:14:36 +0900

    hotfix: Swagger CORS 오류 해결

--- application/src/main/java/way/application/controller/HealthCheckController.java
+++ application/src/main/java/way/application/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package way.application.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import way.application.core.base.BaseResponse;
 
 @RestController
+
 @Tag(name = "Server Health Check", description = "서버 환경 확인을 위한 URL: https://wlrmadjel.com/actuator/health 반환값 200이면 Server OK")
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://wlrmadjel.com")})
 public class HealthCheckController {
     @GetMapping("/actuator/health")
     public ResponseEntity<BaseResponse> healthCheck() {
