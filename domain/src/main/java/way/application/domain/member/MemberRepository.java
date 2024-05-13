6931c2ad05ef15ffdb59dffacb02aca3aa1b5e6e
lynjun <lynjun97@naver.com>
2024-05-13 23:07:41 +0900

    feat: get 추가

--- domain/src/main/java/way/application/domain/member/MemberRepository.java
+++ domain/src/main/java/way/application/domain/member/MemberRepository.java
@@ -3,4 +3,6 @@ package way.application.domain.member;
 public interface MemberRepository {
 
     void save (Member.SaveMemberRequest request);
+
+    Member.CheckIdResponse get (Member.CheckIdRequest request);
 }
