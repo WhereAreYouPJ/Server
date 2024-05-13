79439ed32ea385ceaa3df844edf37019ba90cacb
lynjun <lynjun97@naver.com>
2024-05-13 23:07:32 +0900

    feat: findByUserId 추가

--- data/src/main/java/way/application/data/member/MemberJpaRepository.java
+++ data/src/main/java/way/application/data/member/MemberJpaRepository.java
@@ -3,7 +3,9 @@ package way.application.data.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
-
+    Optional<MemberEntity> findByUserId(String userId);
 }
