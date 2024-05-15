072abc851785e63f09674381984dda8521fccb75
lynjun <lynjun97@naver.com>
2024-05-15 16:40:28 +0900

    feat : CheckEmailUseCase 생성

---
+++ domain/src/main/java/way/application/domain/member/usecase/CheckEmailUseCase.java
@@ -0,0 +1,18 @@
+package way.application.domain.member.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import way.application.domain.member.Member;
+import way.application.domain.member.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
+public class CheckEmailUseCase {
+
+    private final MemberRepository memberRepository;
+
+    public Member.CheckEmailResponse invoke(Member.CheckEmailRequest request) {
+
+        return memberRepository.findByEmail(request);
+    }
+}
