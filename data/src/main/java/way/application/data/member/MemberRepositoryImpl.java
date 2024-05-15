62c0edf70d89db6a212e0eb96eb7370a040c69b1
lynjun <lynjun97@naver.com>
2024-05-15 22:06:43 +0900

    feat : login 추가

--- data/src/main/java/way/application/data/member/MemberRepositoryImpl.java
+++ data/src/main/java/way/application/data/member/MemberRepositoryImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import way.application.data.utils.ValidateUtils;
+import way.application.domain.jwt.JwtRepository;
 import way.application.domain.member.Member;
 import way.application.domain.member.MemberRepository;
 
@@ -16,6 +17,7 @@ public class MemberRepositoryImpl implements MemberRepository {
     private final MemberMapper memberMapper;
     private final BCryptPasswordEncoder encoder;
     private final ValidateUtils validateUtils;
+    private final JwtRepository jwtRepository;
 
     @Override
     @Transactional
@@ -26,6 +28,7 @@ public class MemberRepositoryImpl implements MemberRepository {
     }
 
     @Override
+    @Transactional
     public Member.CheckIdResponse findByUserId(Member.CheckIdRequest request) {
 
         //예외처리
@@ -35,11 +38,30 @@ public class MemberRepositoryImpl implements MemberRepository {
     }
 
     @Override
+    @Transactional
     public Member.CheckEmailResponse findByEmail(Member.CheckEmailRequest request) {
 
         validateUtils.validateEmail(request.email());
 
         return new Member.CheckEmailResponse(request.email());
+    }
+
+    @Override
+    @Transactional
+    public Member.MemberLoginResponse login(Member.MemberLoginRequest request) {
+
+        // 검증
+        MemberEntity member = validateUtils.validateUserId(request.userId());
+        validateUtils.validatePassword(request.password(), member.getEncodedPassword());
+
+        // fcm 토큰 저장
+        memberJpaRepository.updateByFireBaseTargetToken(request.targetToken());
+
+        // jwt 생성
+        String accessToken = jwtRepository.generateAccessToken(request.userId());
+        String refreshToken = jwtRepository.generateRefreshToken(request.userId());
+
+        return new Member.MemberLoginResponse(accessToken,refreshToken, member.getUserId());
     }
 
     // TODO 로그인 시 MemberEntity firebaseTargetToken 저장 로직 구현 필요
