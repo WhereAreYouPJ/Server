65abb18d5ecd52a2a0c6320ebd018fd15aa0ed24
lynjun <lynjun97@naver.com>
2024-05-15 22:06:18 +0900

    feat : updateByFireBaseTargetToken 추가

--- data/src/main/java/way/application/data/member/MemberJpaRepository.java
+++ data/src/main/java/way/application/data/member/MemberJpaRepository.java
@@ -1,6 +1,7 @@
 package way.application.data.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findByUserId(String userId);
 
     Optional<MemberEntity> findByEmail(String email);
+
+    @Query("update MemberEntity set fireBaseTargetToken =:token ")
+    void updateByFireBaseTargetToken(String token);
 }
