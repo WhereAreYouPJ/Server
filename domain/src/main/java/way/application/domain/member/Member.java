fe8681371bcbcf33054ff5757b69db676dba7228
lynjun <lynjun97@naver.com>
2024-05-23 22:22:11 +0900

    feat: VerifyPasswordCodeRequest 추가

--- domain/src/main/java/way/application/domain/member/Member.java
+++ domain/src/main/java/way/application/domain/member/Member.java
@@ -104,5 +104,17 @@ public class Member {
 
     }
 
+    public record VerifyPasswordCodeRequest(
+            @NotBlank(message = "userId를 입력해주세요")
+            @Pattern(regexp = "^[a-z][a-z0-9]{4,11}$", message = "userId는 영문 소문자로 시작하고, 5~12자 길이의 영문 소문자와 숫자만 사용 가능합니다.")
+            String userId,
 
+            @NotBlank(message = "email를 입력해주세요")
+            @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "유효한 이메일 형식이어야 합니다.")
+            String email,
+
+            String code
+    ) {
+
+    }
 }
