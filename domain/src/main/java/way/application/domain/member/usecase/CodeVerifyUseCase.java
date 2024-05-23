74eaeb93180bb4b3947d1302914dd0fa17d31e11
lynjun <lynjun97@naver.com>
2024-05-23 21:37:35 +0900

    feat : CodeVerifyUseCase 생성

---
+++ domain/src/main/java/way/application/domain/member/usecase/CodeVerifyUseCase.java
@@ -0,0 +1,19 @@
+package way.application.domain.member.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import way.application.domain.member.Member;
+import way.application.domain.member.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
+public class CodeVerifyUseCase {
+
+    private final MemberRepository memberRepository;
+
+    public void invoke(Member.CodeVerifyRequest request) {
+
+        memberRepository.verifyCode(request);
+    }
+
+}
