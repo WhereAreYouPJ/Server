883d2c0fb06f39bba9ab9cbd8fe8c29e3ee999be
lynjun <lynjun97@naver.com>
2024-05-23 22:52:49 +0900

    feat: ResetPasswordUseCase 생성

---
+++ domain/src/main/java/way/application/domain/member/usecase/ResetPasswordUseCase.java
@@ -0,0 +1,18 @@
+package way.application.domain.member.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import way.application.domain.member.Member;
+import way.application.domain.member.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
+public class ResetPasswordUseCase {
+
+    private final MemberRepository memberRepository;
+
+    public void invoke(Member.PasswordResetRequest request) {
+
+        memberRepository.resetPassword(request);
+    }
+}
